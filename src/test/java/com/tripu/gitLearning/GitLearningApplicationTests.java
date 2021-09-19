3350e13d8b07a31f0919718b0ca353e79378a14b
tripumani <tripu2017@gmail.com>
2021-09-19 01:37:57 -0700

    master commit

---
+++ src/test/java/com/tripu/gitLearning/GitLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripu.gitLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
